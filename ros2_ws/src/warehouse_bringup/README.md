0877e6e46396c3e05c5b965b063b17b2e8dc68b3
akshatmdnr-glitch <akshatmdnr@gmail.com>
2026-08-03 01:01:36 +0530

    feat(WDT-002): add warehouse_bringup ROS2 Python package
    Create minimal ament_python package with standard structure,
    empty launch/ directory, and no executable nodes.

---
+++ ros2_ws/src/warehouse_bringup/README.md
@@ -0,0 +1,3 @@
+# warehouse_bringup
+
+Bringup scripts and launch files for the Warehouse Digital Twin.
---
+++ ros2_ws/src/warehouse_bringup/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+package_name = 'warehouse_bringup'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Akshat',
+    maintainer_email='akshat@example.com',
+    description='Bringup scripts and launch files for the Warehouse Digital Twin.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+)
